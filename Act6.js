8b8032eab62ee41e21b31be5c6d9062003be9c62
Diego <91744554+msDiego@users.noreply.github.com>
2022-10-10 21:20:14 +0200

    Ejercicios JS

--- Act6.js
+++ Act6.js
@@ -1,7 +1,7 @@
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
     'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
-var DNI = "43226225"
+var DNI = ParseInt(prompt("Introduce número DNI"));
 
 console.log(DNI.length)
 
